c077eb5a73eaf80d57d7eb096c5133ae0475e971
Sloan Finger <hello@sloanfinger.com>
2025-09-25 23:29:41 -0400

    quick fixes

--- src/env.js
+++ src/env.js
@@ -16,7 +16,7 @@ export const env = createEnv({
         new URL(
           path,
           process.env.NODE_ENV === "development"
-            ? `http://localhost:${process.env.PORT}`
+            ? `http://localhost:3000`
             : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
         ).toString(),
       ),
